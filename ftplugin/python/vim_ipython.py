56fa1d33a2cd1397d41cfe0e74a36d830333434b
Jacob Niehus <jacob.niehus@gmail.com>
2014-12-04 19:29:40 -0700

    Change default options

--- ftplugin/python/vim_ipython.py
+++ ftplugin/python/vim_ipython.py
@@ -1,7 +1,7 @@
-reselect = False            # reselect lines after sending from Visual mode
-show_execution_count = True # wait to get numbers for In[43]: feedback?
-monitor_subchannel = True   # update vim-ipython 'shell' on every send?
-run_flags= "-i"             # flags to for IPython's run magic when using <F5>
+reselect = False             # reselect lines after sending from Visual mode
+show_execution_count = False # wait to get numbers for In[43]: feedback?
+monitor_subchannel = False   # update vim-ipython 'shell' on every send?
+run_flags= "-i"              # flags to for IPython's run magic when using <F5>
 current_line = ''
 
 try:
