354310b43d9a9e5609a6bfff08091ccbef235593
Bernard Roesler <bernard.roesler@gmail.com>
2018-02-20 16:11:15 -0500

    Eliminate sys.stdout.flush patch. Not needed in newer vim (~2011+)

--- ftplugin/python/vim_ipython.py
+++ ftplugin/python/vim_ipython.py
@@ -66,19 +66,6 @@ current_stdin_prompt = {}
 # get around unicode problems when interfacing with vim
 vim_encoding = vim.eval('&encoding') or 'utf-8'
 
-try:
-    sys.stdout.flush
-except AttributeError:
-    # IPython complains if stderr and stdout don't have flush
-    # this is fixed in newer version of Vim
-    class WithFlush(object):
-        def __init__(self,noflush):
-            self.write=noflush.write
-            self.writelines=noflush.writelines
-        def flush(self):pass
-    sys.stdout = WithFlush(sys.stdout)
-    sys.stderr = WithFlush(sys.stderr)
-
 def vim_variable(name, default=None):
     exists = int(vim.eval("exists('%s')" % name))
     return vim.eval(name) if exists else default
